5d70f31b71efeb52742751e0ebe49fceb58e5e23
qiye <1710797241@qq.com>
2024-01-15 17:38:10 +0800

    feat: 类继承

---
+++ Class/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Basketball basketball = new Basketball(9);
+Console.WriteLine($"Hello, World!{basketball.GetSize()}");
+public class Ball
+{
+  private int size;
+  public Ball(int _size)
+  {
+    size = _size;
+  }
+  public int GetSize()
+  {
+    return size;
+  }
+}
+public class Basketball : Ball
+{
+  public Basketball(int _size) : base(_size)
+  {
+
+  }
+}
