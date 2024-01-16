47c7596a92c3b51de49f59c888cb7f1dd2b89e04
qiye <1710797241@qq.com>
2024-01-16 16:11:47 +0800

    feat: Generic<T>

---
+++ Generic/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+GenericList<TestGeneric> genericList = new GenericList<TestGeneric>();
+genericList.add(new TestGeneric());
+GenericList<int> g = new GenericList<int>();
+g.add(100);
+public class GenericList<T>
+{
+  public void add(T input)
+  {
+
+  }
+}
+public class TestGeneric
+{
+
+}
