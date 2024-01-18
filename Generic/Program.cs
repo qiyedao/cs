ad278debfc58257b2060f053bf8ac20397c52bdf
qiye <1710797241@qq.com>
2024-01-18 16:53:57 +0800

    feat: 匿名类型

--- Generic/Program.cs
+++ Generic/Program.cs
@@ -4,6 +4,9 @@ GenericList<TestGeneric> genericList = new GenericList<TestGeneric>();
 genericList.add(new TestGeneric());
 GenericList<int> g = new GenericList<int>();
 g.add(100);
+// 匿名类型
+var a = new { name = "999", age = "99" };
+Console.WriteLine(a.name);
 public class GenericList<T>
 {
   public void add(T input)
--- Generic/obj/Debug/net8.0/Generic.AssemblyInfo.cs
+++ Generic/obj/Debug/net8.0/Generic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Generic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+255899534deb6e70c35b687f4064518518217633")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47c7596a92c3b51de49f59c888cb7f1dd2b89e04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Generic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Generic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
