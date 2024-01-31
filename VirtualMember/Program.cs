9e6e7ca6f90d28e12d40f410e7f2f6a4ac9fdb67
qiye <1710797241@qq.com>
2024-01-31 16:56:40 +0800

    feat: 重写virtual 方法

---
+++ VirtualMember/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Square square = new Square();
+square.draw();
+Shape shape = new Square();
+shape.draw();
+Shape shape1 = new Shape();
+shape1.draw();
+Shape shape2 = (Shape)new Square();
+shape2.draw();
+public class Shape
+{
+  public virtual void draw()
+  {
+    Console.WriteLine("Base Shape");
+  }
+}
+public class Square : Shape
+{
+  public override void draw()
+  {
+    Console.WriteLine("Draw Square");
+  }
+}
