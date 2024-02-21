3001b614d85af0b2ac8a12391392faca913db834
qiye <1710797241@qq.com>
2024-02-21 15:21:47 +0800

    feat: 异常处理

---
+++ Exception/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+void TestFinally()
+{
+  throw new CustomException("CustomException");
+}
+try
+{
+  TestFinally();
+}
+catch (CustomException ex)
+{
+  Console.WriteLine(ex.ToString());
+}
+finally
+{
+  Console.WriteLine("done");
+}
+public class CustomException : Exception
+{
+  public CustomException(string message)
+  {
+
+  }
+}
