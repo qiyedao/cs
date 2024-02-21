298f3b7395758fe985e1b8c008cea8bf846beaec
qiye <1710797241@qq.com>
2024-02-21 13:29:12 +0800

    feat: 模式匹配

---
+++ ModeMatch/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int? a = 1;
+int? b = null;
+if (a is int)
+{
+  Console.WriteLine("int");
+}
+if (a is not null)
+{
+  Console.WriteLine("not null");
+}
+if (b is null)
+{
+  Console.WriteLine("null");
+}
